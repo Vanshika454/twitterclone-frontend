a78d7bfeeba578b085667e74656bb114bc64f307
Vanshika454 <110763792+Vanshika454@users.noreply.github.com>
2024-06-12 09:30:17 +0530

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BACKEND,
+    baseURL: http://localhost:3033.REACT_APP_BACKEND,
     headers: {
         'Content-type': 'application/json',
     },
